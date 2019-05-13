b177534ec5bda03d0b659149592390149fee41e7
Nicolas COURSOL <ncoursol@e2r7p10.42.fr>
2019-05-13 19:48:18 +0200

    ajout printf.h convert.c modifs

--- ft_convert.c
+++ ft_convert.c
@@ -6,21 +6,115 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 15:33:24 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/05/13 19:42:09 by dberger          ###   ########.fr       */
+/*   Updated: 2019/05/13 19:45:55 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void		ft_flags(char *str, t_printf save, int *i)
+{
+	if (str[*i] == '#')
+		save.flags = save.flags | F_HASH;
+	else if (str[*i] == '0')
+		save.flags = save.flags | F_ZERO;
+	else if (str[*i] == '+')
+		save.flags = save.flags | F_MINUS;
+	else if (str[*i] == ' ')
+		save.flags = save.flags | F_SPACE;
+	else if (str[*i] == '-')
+		save.flags = save.flags | F_MINUS;
+	while (str[*i] != '#' && str[*i] != '0' && str[*i] != '+'
+			&& str[*i] != ' ' && str[*i] != '-')
+		*i += 1;
+}
+
+int			ft_width(char *str, t_printf save, int *i)
+{
+	if (str[*i] >= '0' && str[*i] <= '9')
+	{
+		while (str[*i] >= '0' && str[*i] <= '9')
+		{
+			save.width = (save.width * 10) + ft_atoi((char*)str[*i]);
+			*i += 1;
+		}
+	}
+	if (str[*i] == '.')
+	{
+		*i += 1;
+		if ((str[*i] <= '0' || str[*i] >= '9') && str[*i] != 'd'
+				&& str[*i] != 'i' && str[*i] != 'o' && str[*i] != 'u'
+				&& str[*i] != 'x' && str[*i] != 'X' && str[*i] != 'f'
+				&& str[*i] != 'c' && str[*i] != 's' && str[*i] != 'p')
+			return (0);
+		while (str[*i] >= '0' && str[*i] <= '9')
+		{
+			save.precision = (save.precision * 10) + ft_atoi((char*)str[*i]);
+			*i += 1;
+		}
+	}
+}
+
+int			ft_modif(char *str, t_printf save, int *i)
+{
+	if (str[*i] == 'h' && str[*i + 1] == 'h')
+	{
+		save.modif = save.modif | M_HH;
+		*i += 1;
+	}
+	else if (str[*i] == 'h' && str[*i + 1] != 'h')
+		save.modif = save.modif | M_H;
+	else if (str[*i] == 'l' && str[*i + 1] == 'l')
+	{
+		save.modif = save.modif | M_LL;
+		*i += 1;
+	}
+	else if (str[*i] == 'l' && str[*i + 1] != 'l')
+		save.modif = save.modif | M_L;
+	else if (str[*i] == 'L')
+		save.modif = save.modif | M_BIGL;
+	*i += 1;
+	if (str[*i] != 'd' && str[*i] != 'i' && str[*i] != 'o' && str[*i] != 'u'
+			&& str[*i] != 'x' && str[*i] != 'X' && str[*i] != 'f'
+			&& str[*i] != 'c' && str[*i] != 's' && str[*i] != 'p')
+		return (0);
+}
+
+void		ft_format(char c, t_printf save, va_list, int *j)
+{
+	if (c == 'c' || c == 's' || c == 'p')
+		ft_format_csp(save, ap, j);
+	else if (c == 'd' || c == 'i')
+		ft_format_di(save, ap, j);
+	else if (c == 'x' || c == 'X')
+		ft_format_xX(save, ap, j);
+	else if (c == 'f')
+		ft_format_f(save, ap, j);
+	else if (c == 'o')
+		ft_format_o(save, ap, j);
+	else if (c == 'u')
+		ft_format_u(save, ap, j);
+}
+
 t_printf	ft_convert(t_printf save, char *str, va_list ap, int *j)
 {
-	int	type;
-	int		i;
+	int			i;
+	t_printf	save;
 
 	i = 0;
-	if (*j == BUFF_SIZE && (*j = 0) == 0)
-		printf("%s", save.buf);
-	if (str[i] == 'd')
-		save = ft_format_di(save, ap, j);
+	save.flags = 0;
+	save.width = 0;
+	save.precision = 0;
+	save.modif = 0;
+	i++;
+	ft_flags(str, save, *i);
+	if (ft_width(str, save, *i) == 0 || ft_modif(str, save, *i) == 0)
+	{
+		save.buf[j] = '%';
+		*j += 1;
+		return (save);
+	}
+	save.index = i + 1;
+	ft_format(str[i], save, ap, *j);
 	return (save);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 18:04:24 by dberger           #+#    #+#             */
-/*   Updated: 2019/05/13 19:41:52 by dberger          ###   ########.fr       */
+/*   Updated: 2019/05/13 19:47:10 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,13 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-# define BUFF_SIZE 555
+# define BUFF_SIZE 1
 
-# define F_HASH		(1 << 0)
-# define F_PLUS		(1 << 1)
+# define F_SPACE	(1 << 0)
+# define F_HASH		(1 << 1)
 # define F_MINUS	(1 << 2)
-# define F_SPACE	(1 << 3)
+# define F_PLUS		(1 << 3)
 # define F_ZERO		(1 << 4)
-# define P_POINT	(1 << 5)
 
 # define M_HH		(1 << 0)
 # define M_H		(1 << 1)
@@ -38,13 +37,14 @@ typedef struct		s_printf
 {
 	int				flags;
 	int				width;
+	int				precision;
 	int				modif;
-	int				type;
-	char			buf[BUFF_SIZE + 1];
+	int				index;
+	char			buf[BUFF_SIZE];
 }					t_printf;
 
 int					ft_printf(const char *restrict format, ...);
-t_printf			ft_convert(t_printf save, char *str, va_list ap, int *j);
+t_printf			ft_convert(t_printf save, char *str, va_list ap, int *j)
 t_printf			ft_format_di(t_printf save, va_list ap, int *j);
 
 #endif
