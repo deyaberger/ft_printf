c94fa8726095616de83f255556cbfa4fe16dcd58
Deya BERGER <dberger@e2r7p12.42.fr>
2019-05-13 13:38:03 +0200

    format di et convert, debut de structure

--- ft_convert.c
+++ ft_convert.c
@@ -6,15 +6,42 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 15:33:24 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/05/12 18:18:55 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/05/13 13:37:32 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char	*ft_convert(char *str, va_list ap)
+char	*ft_convert(char *str, va_list ap, char *buf)
 {
-	(void)ap;
-	(void)str;
+	int			i;
+	t_printf 	save;
+
+	i = 0;
+	if (!(save = (t_printf)malloc(sizeof(t_printf))))
+		return (NULL);
+	save.flags = 0;
+	save.width = 0;
+	save.precision = 0;
+	save.modif = 0;
+	i++;
+	while (str[i] != d && str[i] != i && str[i] && str[i] != o && str[i] != u 
+		&& str[i] != x && str[i] != X && str[i] != f && str[i] != c && str[i] != s
+		&& str[i] != p)
+	{
+		if (str[i] == '#')
+			save.flags = save.flgas | F_HASH;
+		if (str[i] == '0')
+			save.flags = save.flags | F_ZERO;
+		if (str[i] == '+')
+			save.flags = save.flags | F_MINUS;
+		if (str[i] == ' ')
+			save.flags = save.flags | F_SPACE;
+		if (str[i] == '-')
+			save.flags = save.flags | F_MINUS;
+		i++;
+	}
+	if (str[i] == 'd')
+	buf = ft_format_di(save, ap, buf);
 	return ("RESULTAT");
 }
