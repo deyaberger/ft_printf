73d00397b7e8b94412f4db7fef760da704a62cb9
Deya BERGER <dberger@e2r7p14.42.fr>
2019-05-15 16:36:10 +0200

    format di avec modif h

--- ft_format_di.c
+++ ft_format_di.c
@@ -6,11 +6,23 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 13:17:49 by dberger           #+#    #+#             */
-/*   Updated: 2019/05/15 15:15:21 by dberger          ###   ########.fr       */
+/*   Updated: 2019/05/15 16:34:09 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+short	ft_modif_di(int type)
+{
+	short	sh;
+
+	if (type == 32768)
+		return (-32768);
+	if (type > 32768 || type < -32768)
+		return (32767);
+	sh = type;
+	return (sh);
+}
 
 t_printf	ft_width_di(t_printf save, int *j, int type)
 {
@@ -58,11 +70,18 @@ t_printf	ft_ditoa(t_printf save, int *j, int type)
 {
 	int		s;
 	int		count;
+	long	t;
 
-	s = ft_sizenb(type);
+	t = type;
+	s = ft_sizenb(t);
 	count = s;
 	save.buf[*j + s] = '\0';
-	if (type == 0)
+	if ((save.modif & M_H))
+	{
+		t = ft_modif_di(type);
+		s = ft_sizenb(t);
+	}
+	if (t == 0)
 	{
 		if (*j == BUFF_SIZE && (*j = 0) == 0)
 			write (1, &save.buf, BUFF_SIZE);
@@ -70,7 +89,7 @@ t_printf	ft_ditoa(t_printf save, int *j, int type)
 		*j += 1;
 		return (save);
 	}
-	if (type < 0)
+	if (t < 0)
 	{
 		if (*j == BUFF_SIZE && (*j = 0) == 0)
 			write (1, &save.buf, BUFF_SIZE);
@@ -79,13 +98,12 @@ t_printf	ft_ditoa(t_printf save, int *j, int type)
 			save.buf[*j] = '-';
 			*j += 1;
 		}
-		type = -type;
+		t = -t;
 		s--;
-		count--;
 	}
 	if (*j == BUFF_SIZE && (*j = 0) == 0)
 		write (1, &save.buf, BUFF_SIZE);
-	if (type > 0 && !save.precision && (save.flags & F_PLUS))
+	if (t > 0 && !save.precision && (save.flags & F_PLUS))
 	{	
 		save.buf[*j] = '+';
 		*j += 1;
@@ -95,9 +113,9 @@ t_printf	ft_ditoa(t_printf save, int *j, int type)
 	{
 		if (*j + s == BUFF_SIZE && (*j = 0) == 0)
 			write (1, &save.buf, BUFF_SIZE);
-		save.buf[*j + s] = (type % 10 + '0');
+		save.buf[*j + s] = (t % 10 + '0');
 		s--;
-		type = type / 10;
+		t = t / 10;
 	}
 	*j = *j + count;
 	return (save);
