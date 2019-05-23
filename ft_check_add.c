9e5898d2cf88913c921ba2ede786f538c58edf79
Deya BERGER <dberger@e2r7p10.42.fr>
2019-05-23 16:26:16 +0200

    ft_check a la norme

--- ft_check_add.c
+++ ft_check_add.c
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 16:37:21 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/05/22 16:39:36 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/05/23 16:24:34 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 t_printf	ft_check_add(t_printf save, int *j, char c)
 {
 	if (*j == BUFF_SIZE && (*j = 0) == 0)
-		 write(1, &save.buf, BUFF_SIZE);
+		write(1, &save.buf, BUFF_SIZE);
 	save.buf[*j] = c;
+	save.buf[*j + 1] = '\0';
 	*j += 1;
 	return (save);
 }
