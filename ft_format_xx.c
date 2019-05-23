507db5065f5b08687150dfebc810827fab455c4d
Nicolas COURSOL <ncoursol@e2r6p8.42.fr>
2019-05-23 12:13:35 +0200

    modif xx

--- ft_format_xx.c
+++ ft_format_xx.c
@@ -6,9 +6,11 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:34:43 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/05/23 11:36:14 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/05/23 12:01:36 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 t_printf	ft_format_xx(t_printf save, va_list ap, int *j, char c)
 {
