88ac7255b62ba6260a51765a969d2ad975e44296
Nicolas COURSOL <ncoursol@e2r13p5.42.fr>
2019-05-27 15:55:32 +0200

    ajout format_xx norme

--- ft_format_xx.c
+++ ft_format_xx.c
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:34:43 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/05/27 11:54:41 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/05/27 15:53:10 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,26 +32,24 @@ int			ft_len(t_printf save, unsigned long long nb, int mode)
 
 	i = 63;
 	if (nb != 0)
-	{
 		while ((nb >> i | nb >> (i - 1) | nb >> (i - 2) | nb >> (i - 3)) == 0)
 			i -= 4;
-		while (i % 4 != 0)
-			i++;
-	}
-	else
-		(save.precision == 0 && (save.flags & F_POINT)) ? (i = -1) : (i = 4);
+	while (i % 4 != 0)
+		i++;
+	if (nb == 0)
+		i = ((save.precision == 0 && (save.flags & F_POINT)) ? -1 : 4);
 	if (mode == 0)
 	{
-		(k = (save.precision - (i / 4))) < 0 ? (k = 0) : 0;
-		(s = (save.width - ((i / 4))) - k) < 0 ? (s = 0) : 0;
+		k = ((save.precision - (i / 4)) < 0 ? 0 : save.precision - (i / 4));
+		s = ((save.width - ((i / 4) - k)) < 0 ? 0 : save.width - ((i / 4) - k));
 		(nb == 0) ? (s += 2) : 0;
-		(save.flags & F_HASH)
-		|| (save.precision == 0 && (save.flags & F_POINT)) ? s -= 2 : 0;
+		s = (save.flags & F_HASH)
+			|| (save.precision == 0 && (save.flags & F_POINT)) ? s - 2 : s;
 	}
 	else if (mode == 1)
-		(s = (save.precision - (i / 4))) < 0 ? (s = 0) : 0;
+		s = ((save.precision - (i / 4)) < 0 ? 0 : save.precision - (i / 4));
 	else
-		((i - 1) < 0) ? (s = -1) : (s = i - 1);
+		s = (((i - 1) < 0) ? -1 : (i - 1));
 	return (s);
 }
 
@@ -59,8 +57,13 @@ t_printf	ft_print_h(t_printf save, int *j, unsigned long long nb, int mode)
 {
 	int		i;
 	int		c;
+	int		s2;
 
 	c = 0;
+	s2 = ft_len(save, nb, 1);
+	if (save.precision != 0)
+		while (s2-- != 0)
+			save = ft_check_add(save, j, '0');
 	i = ft_len(save, nb, 2);
 	while (i > -1)
 	{
@@ -81,29 +84,21 @@ t_printf	ft_print_h(t_printf save, int *j, unsigned long long nb, int mode)
 t_printf	ft_form_xx(t_printf save, int *j, unsigned long long nb, int mode)
 {
 	int		s;
-	int		s2;
 
 	s = ft_len(save, nb, 0);
-	s2 = ft_len(save, nb, 1);
-	if ((save.flags & F_MINUS) == F_MINUS)
+	if ((save.flags & F_MINUS))
 	{
 		save = ft_print_ox(save, j, nb, mode);
-		if (save.precision != 0)
-			while (s2-- != 0)
-				save = ft_check_add(save, j, '0');
 		save = ft_print_h(save, j, nb, mode);
 	}
-	if (save.width != 0 && (save.flags & F_ZERO) != F_ZERO)
+	if (save.width != 0 && !(save.flags & F_ZERO))
 		while (s-- != 0)
 			save = ft_check_add(save, j, ' ');
-	if ((save.flags & F_MINUS) != F_MINUS)
+	if (!(save.flags & F_MINUS))
 	{
 		save = ft_print_ox(save, j, nb, mode);
-		if (save.width != 0 && (save.flags & F_ZERO) == F_ZERO)
+		if (save.width != 0 && (save.flags & F_ZERO))
 			while (s-- != 0)
-				save = ft_check_add(save, j, '0');
-		if (save.precision != 0)
-			while (s2-- != 0)
 				save = ft_check_add(save, j, '0');
 		save = ft_print_h(save, j, nb, mode);
 	}
@@ -114,19 +109,19 @@ t_printf	ft_format_xx(t_printf save, va_list ap, int *j, char c)
 {
 	unsigned long long	number;
 
-	if (save.modif && (save.modif & M_HH) == M_HH)
+	if (save.modif && (save.modif & M_HH))
 		number = (unsigned char)va_arg(ap, int);
-	else if (save.modif && (save.modif & M_H) == M_H)
+	else if (save.modif && (save.modif & M_H))
 		number = (unsigned short)va_arg(ap, int);
-	else if (save.modif && (save.modif & M_L) == M_L)
+	else if (save.modif && (save.modif & M_L))
 		number = va_arg(ap, unsigned long);
-	else if (save.modif && (save.modif & M_LL) == M_LL)
+	else if (save.modif && (save.modif & M_LL))
 		number = va_arg(ap, unsigned long long);
 	else
 		number = va_arg(ap, unsigned int);
-	if ((save.flags & F_MINUS) == F_MINUS && (save.flags & F_ZERO) == F_ZERO)
+	if ((save.flags & F_MINUS) && (save.flags & F_ZERO))
 		save.flags -= F_ZERO;
-	if (save.precision != 0 && (save.flags & F_ZERO) == F_ZERO)
+	if (save.precision != 0 && (save.flags & F_ZERO))
 		save.flags -= F_ZERO;
 	if (c == 'x')
 		save = ft_form_xx(save, j, number, (int)'a');
