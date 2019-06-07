1316f05030b5a88e52e634d26e21125d8742ffff
Deya BERGER <dberger@e3r7p12.42.fr>
2019-06-07 14:38:09 +0200

    ft_format_f a la norme

--- ft_format_f.c
+++ ft_format_f.c
@@ -6,13 +6,13 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 13:17:49 by dberger           #+#    #+#             */
-/*   Updated: 2019/06/07 11:46:48 by dberger          ###   ########.fr       */
+/*   Updated: 2019/06/07 14:37:32 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-t_printf	ft_w_add_f(t_printf save, int *j, int count, char *fix, char *nb)
+t_printf	ft_w_add_f(t_printf save, int *j, int count, char *fix)
 {
 	long	s;
 	int		w;
@@ -32,8 +32,6 @@ t_printf	ft_w_add_f(t_printf save, int *j, int count, char *fix, char *nb)
 			save = ft_check_add(save, j, '0');
 		return (save);
 	}
-	if (fix[0] == '0' && nb[0] == '0' && !(save.pre) && (save.flags & F_POINT))
-		save = ft_check_add(save, j, ' ');
 	while (s < w && (count--) > 0)
 		save = ft_check_add(save, j, ' ');
 	return (save);
@@ -43,17 +41,14 @@ t_printf	ft_width_f(t_printf save, int *j, char *fix, char *nb)
 {
 	int		count;
 	long	s;
-	int		w;
 
-	w = save.width;
-	if ((!(save.pre) && !(save.flags & F_POINT)) || (save.pre == 6))
+	s = ft_strlen(fix) + save.pre + 1;
+	if (!(save.pre) && !(save.flags & F_POINT))
 		s = ft_strlen(fix) + 7;
-	else
-		s = ft_strlen(fix) + save.pre + 1;
-	count = w - s;
-	if (save.pre && w > save.pre && s < save.pre)
-		count = w - save.pre;
-	if (save.pre && s < save.pre && w <= save.pre)
+	count = save.width - s;
+	if (save.pre && save.width > save.pre && s < save.pre)
+		count = save.width - save.pre;
+	if (save.pre && s < save.pre && save.width <= save.pre)
 		return (save);
 	if (save.flags & F_PLUS && fix[0] != '-')
 	{
@@ -64,23 +59,23 @@ t_printf	ft_width_f(t_printf save, int *j, char *fix, char *nb)
 	if ((fix[0] != '-' && (save.flags & F_SPACE) && !(save.flags & F_PLUS))
 		|| (fix[0] == '-' && save.pre))
 		count--;
-	save = ft_w_add_f(save, j, count, fix, nb);
+	if (fix[0] == '0' && nb[0] == '0' && !(save.pre) && (save.flags & F_POINT))
+		count++;
+	save = ft_w_add_f(save, j, count, fix);
 	return (save);
 }
 
 t_printf	ft_format_f(t_printf save, va_list ap, int *j)
 {
 	char	*fix;
-	char 	*fix2;
+	char	*fix2;
 	char	*nb;
-	char	*nb2;	
+	char	*nb2;
 
 	(void)ap;
-	fix2 = "0";
+	fix2 = "6";
 	fix = ft_strndup(fix2, 4098);
-	nb2 = "0";
-//	nb2 = "62399999999999966604491419275291264057159423828125";
-//	nb2 = "6989999999996143742464482784271240234375";
+	nb2 = "62399999999999966604491419275291264057159423828125";
 	nb = ft_strndup(nb2, 4098);
 	if (fix[0] != '-' && (save.flags & F_SPACE) && !(save.flags & F_PLUS))
 		save = ft_check_add(save, j, ' ');
