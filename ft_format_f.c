7a59c1ffa82f8214c025dfcc9e9858fc4c45e931
Deya BERGER <dberger@e3r7p12.42.fr>
2019-06-07 11:49:23 +0200

    format f good sauf norme

--- ft_format_f.c
+++ ft_format_f.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 13:17:49 by dberger           #+#    #+#             */
-/*   Updated: 2019/06/06 11:59:56 by dberger          ###   ########.fr       */
+/*   Updated: 2019/06/07 11:46:48 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,15 @@ t_printf	ft_w_add_f(t_printf save, int *j, int count, char *fix, char *nb)
 		s = ft_strlen(fix) + save.pre + 1;
 	if (fix[0] == '-' && save.pre > 0 && save.pre < s && w > s)
 		count++;
-	if (fix[0] == '-' && (save.flags & F_ZERO) && !(save.flags & F_MINUS)
-		&& !(save.pre))
+	if (fix[0] == '-' && (save.flags & F_ZERO) && !(save.flags & F_MINUS))
 		save = ft_check_add(save, j, '-');
-	if ((save.flags & F_ZERO) && !(save.pre) && !(save.flags & F_MINUS)
-		&& !(save.flags & F_POINT))
+	if ((save.flags & F_ZERO) && !(save.flags & F_MINUS))
 	{
 		while (s < w && (count--) > 0)
 			save = ft_check_add(save, j, '0');
 		return (save);
 	}
-	if (fix[0] == '0' && nb[0] == '0' && !(save.pre) && save.flags & F_POINT)
+	if (fix[0] == '0' && nb[0] == '0' && !(save.pre) && (save.flags & F_POINT))
 		save = ft_check_add(save, j, ' ');
 	while (s < w && (count--) > 0)
 		save = ft_check_add(save, j, ' ');
@@ -70,32 +68,6 @@ t_printf	ft_width_f(t_printf save, int *j, char *fix, char *nb)
 	return (save);
 }
 
-
-t_printf	ft_pre_f(t_printf save, int *j, char *fix, char *nb)
-{
-	long	s;
-	int		p;
-
-	if ((!(save.pre) && !(save.flags & F_POINT)) || (save.pre == 6))
-		s = ft_strlen(fix) + 7;
-	else
-		s = ft_strlen(fix) + save.pre + 1;
-	p = save.pre;
-	if (fix[0] != '-' && (save.flags & F_PLUS))
-		save = ft_check_add(save, j, '+');
-	if (s < p)
-	{
-		while (p - s > 0)
-		{
-			save = ft_check_add(save, j, '0');
-			p--;
-		}
-	}
-	if (fix[0] == '0' && nb[0] == '0' && save.pre > 0)
-		save = ft_check_add(save, j, '0');
-	return (save);
-}
-
 t_printf	ft_format_f(t_printf save, va_list ap, int *j)
 {
 	char	*fix;
@@ -104,16 +76,18 @@ t_printf	ft_format_f(t_printf save, va_list ap, int *j)
 	char	*nb2;	
 
 	(void)ap;
-	fix2 = "6";
-	fix = ft_strdup(fix2);
-	nb2 = "62399999999999966604491419275291264057159423828125";
-	nb = ft_strdup(nb2);
+	fix2 = "0";
+	fix = ft_strndup(fix2, 4098);
+	nb2 = "0";
+//	nb2 = "62399999999999966604491419275291264057159423828125";
+//	nb2 = "6989999999996143742464482784271240234375";
+	nb = ft_strndup(nb2, 4098);
 	if (fix[0] != '-' && (save.flags & F_SPACE) && !(save.flags & F_PLUS))
 		save = ft_check_add(save, j, ' ');
 	if (save.width && !(save.flags & F_MINUS))
 		save = ft_width_f(save, j, fix, nb);
-	if (save.pre)
-		save = ft_pre_f(save, j, fix, nb);
+	if (save.pre && fix[0] != '-' && (save.flags & F_PLUS))
+		save = ft_check_add(save, j, '+');
 	save = ft_ftoa(save, j, fix, nb);
 	if (save.width && (save.flags & F_MINUS))
 		save = ft_width_f(save, j, fix, nb);
--- ft_ftoa.c
+++ ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 08:43:36 by dberger           #+#    #+#             */
-/*   Updated: 2019/06/06 11:59:27 by dberger          ###   ########.fr       */
+/*   Updated: 2019/06/07 11:48:59 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@
 char		*ft_n_round(char *str, int n)
 {
 	int		ret;
-	int		s;
 	char 	c;
 
 	ret = 0;
-	s = n;
 	str[n + 1] = '\0';
 	str[n] += 1;
 	if (str[n] > '9')
@@ -55,76 +53,90 @@ char		*ft_n_round(char *str, int n)
 	return (str);
 }
 
-char		*ft_fix(t_printf save, char *fix, char *nb)
+char		*ft_fix_nb(t_printf save, char *fix, char *nb, int mode)
 {
 	int		s;
-
-	s = ft_strlen(fix);
-	if (s > 0)
-		s--;
-	if (nb[0] >= '5' && !(save.pre) && (save.flags & F_POINT))
-		fix = ft_n_round(fix, s);
-	return (fix);
-}
-
-char		*ft_nb(t_printf save, char *nb)
-{
 	int		p;
 	int		i;
+	int		z;
 
+	s = ft_strlen(fix);
 	p = save.pre;
+	z = save.width;
 	i = 0;
+	if (s > 0)
+		s--;
 	if ((!(save.pre) && !(save.flags & F_POINT)))
-		p = 5;
-	nb = ft_n_round(nb, p);
-	if (nb[0] == '0')
+		p = 6;
+	if (mode == 1 && nb[0] >= '5' && !(save.pre) && (save.flags & F_POINT))
+		fix = ft_n_round(fix, s);
+	if (mode == 2)
 	{
-		while (i < p)
+		if (nb[p] >= '5' && p >= 1)
+			nb = ft_n_round(nb, (p - 1));
+		if (nb[0] == '0')
 		{
-			nb[i] = '0';
-			nb[i + 1] = '\0';
-			i++;
+			while (i < p)
+			{
+				nb[i] = '0';
+				nb[i + 1] = '\0';
+				i++;
+			}
 		}
 	}
-	return (nb);
+	return (mode == 1 ? fix : nb);
 }
-
 
 t_printf	ft_ftoa(t_printf save, int *j, char *fix, char *nb)
 {
 	int 	i;
 	int		p;
+	int		s;
 
 	p = save.pre;
 	i = 0;
-	fix = ft_fix(save, fix, nb);
+	s = ft_strlen(nb);
 	if (nb[0] == '\0')
 	{
-		fix[i] = '0';
-		fix[i + 1] = '\0';
 		nb[0] = '0';
 		nb[1] = '\0';
 	}
+	if (fix[0] == '\0')
+	{
+		fix[0] = '0';
+		fix[1] = '\0';
+	}
+	fix = ft_fix_nb(save, fix, nb, 1);
+	if (fix[0] == '-' && (save.width) && (save.flags & F_ZERO))
+		i = 1;
 	while (fix[i])
 	{
 		save = ft_check_add(save, j, fix[i]);
 		i++;
 	}
 	i = 0;
-	if ((!(save.pre) && !(save.flags & F_POINT)) || (save.pre > 0) || (save.flags & F_HASH))
+	if ((!(save.pre) && !(save.flags & F_POINT)) || (save.pre > 0) 
+		|| (save.flags & F_HASH))
 	{
 		save = ft_check_add(save, j, '.');
-	if ((!(save.pre) && !(save.flags & F_POINT)) || (save.pre > 0))
-	{
-		nb = ft_nb(save, nb);
-		if ((!(save.pre) && !(save.flags & F_POINT)))
-			p = 5;
-		while (nb[i] && i < p)
+		if ((!(save.pre) && !(save.flags & F_POINT)) || (save.pre > 0))
 		{
-			save = ft_check_add(save, j, nb[i]);
-			i++;
+			nb = ft_fix_nb(save, fix, nb, 2);
+			while (s < p || s < 6)
+			{
+				nb[s] = '0';
+				nb[s + 1] = '\0';
+				s++;
+			}
+			nb[s] = '\0';
+			if ((!(save.pre) && !(save.flags & F_POINT)))
+				p = 6;
+			while (nb[i] && i < p)
+			{
+				save = ft_check_add(save, j, nb[i]);
+				i++;
+			}
 		}
-	}
 	}
 	return (save);
 }
