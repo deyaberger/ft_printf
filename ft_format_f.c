564b7adeb0d0c7bd0deb176a9998a9c7aefdcdeb
Deya BERGER <dberger@e3r6p13.42.fr>
2019-06-14 10:43:42 +0200

    format f presque norme, nan inf a la norme, 41 failed test

--- ft_format_f.c
+++ ft_format_f.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 13:37:41 by dberger           #+#    #+#             */
-/*   Updated: 2019/06/13 16:05:12 by dberger          ###   ########.fr       */
+/*   Updated: 2019/06/14 10:32:50 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,76 +60,35 @@ t_printf	ft_width_f(t_printf save, int *j, char *fix, char *nb)
 	if ((fix[0] != '-' && (save.flags & F_SPACE) && !(save.flags & F_PLUS))
 			|| (fix[0] == '-' && save.pre))
 		count--;
-	if (fix[0] == '0' && fix[1] == '\0' && nb[0] == '0' && nb[1] == '\0' && !(save.pre) && (save.flags & F_POINT))
+	if (fix[0] == '0' && fix[1] == '\0' && nb[0] == '0' && nb[1] == '\0'
+			&& !(save.pre) && (save.flags & F_POINT))
 		count++;
 	save = ft_w_add_f(save, j, count, fix);
 	return (save);
 }
 
-t_printf	ft_nan_inf(t_printf save, int *j, char *str)
-{
-	int		i;
-
-	i = 0;
-	if (str[0] == 'i' && (save.flags & F_SPACE) && save.width <= (int)ft_strlen(str) && !(save.flags & F_MINUS) && !(save.min))
-		save = ft_check_add(save, j, ' ');
-	if (str[0] == 'i' && (save.flags & F_PLUS) && !(save.min))
-		str = "+inf";
-	if (str[0] == 'i' && (save.min))
-		str = "-inf";
-	if (!(save.flags & F_POINT))
-		save.pre = -1;
-	if ((str[0] == 'i') && (save.flags & F_SPACE) && (save.flags & F_MINUS) && (save.width))
-	{
-		save = ft_check_add(save, j, ' ');
-		save.width -= 1;
-	}
-	if (save.flags & F_MINUS)
-		while (str[i])
-			save = ft_check_add(save, j, str[i++]);
-	i = ((!str) ? 6 : ft_strlen(str));
-	save.width -= i;
-	i = 0;
-	while (i++ < save.width)
-		save = ft_check_add(save, j, ' ');
-	i = 0;
-	if (!(save.flags & F_MINUS))
-		while (str[i])
-			save = ft_check_add(save, j, str[i++]);
-	return (save);
-}
-
 t_printf	ft_format_f(t_printf save, va_list ap, int *j)
 {
-	long double	f;
-	char		fix[2048];
-	char		nb[2048];
-	unsigned long *var;
+	long double		f;
+	char			fix[2048];
+	char			nb[2048];
+	unsigned long	*var;
 
 	f = va_arg(ap, double);
 	var = (unsigned long*)&f;
 	ft_bzero(fix, 2048);
 	ft_bzero(nb, 2048);
-	if ((0xC000000000000000 == var[0]) && f != 0)
-	{
-		fix[0] = 'n';
-		fix[1] = 'a';
-		fix[2] = 'n';
-		fix[3] = '\0';
-		return (save = ft_nan_inf(save, j, fix));
-	}
-	if ((var[0] == 0x8000000000000000) && ((var[1] & 1) == 1))
-	{
-			if (f < 0)
-				save.min = 1;
-			fix[0] = 'i';
-			fix[1] = 'n';
-			fix[2] = 'f';
-			fix[3] = '\0';
-		return (save = ft_nan_inf(save, j, fix));
-	}
+	save = ft_nan_inf(save, j, var, f);
+	if (save.min != -42)
+		return (save);
+	save.min = 0;
 	ft_float(f, fix, 1);
 	ft_float(f, nb, 2);
+	if (fix[0] == '\0')
+	{
+		fix[0] = '0';
+		fix[1] = '\0';
+	}
 	if (fix[0] != '-' && (save.flags & F_SPACE) && !(save.flags & F_PLUS))
 		save = ft_check_add(save, j, ' ');
 	if (save.width && !(save.flags & F_MINUS))
